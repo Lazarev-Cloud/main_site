b09fc6b26e894d1f7ae20d4861ef0bd3a99c4db8
lazarevtill <lazarevtill@gmail.com>
2024-09-19 16:00:09 +0200

    update

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    // Specify custom output directory if needed
+    distDir: 'out', // This ensures the static files will be output to the "out" folder
+  };
+  
+  module.exports = nextConfig;
+  
