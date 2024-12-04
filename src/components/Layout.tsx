f011160985cfe0c0eb7dc9434b88650be9002b08
lazarevtill <lazarevtill@gmail.com>
2024-12-04 19:12:07 +0100

    add google tag

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,6 +10,7 @@ interface LayoutProps {
 }
 
 const GA_MEASUREMENT_ID = 'G-DT39S71G8J';
+const GTM_ID = 'GTM-NJVJLNBF';
 
 const Layout: React.FC<LayoutProps> = ({ children, title = 'Lazarev.Cloud' }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -28,6 +29,17 @@ const Layout: React.FC<LayoutProps> = ({ children, title = 'Lazarev.Cloud' }) =>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+
       {/* Google Analytics */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
@@ -38,10 +50,19 @@ const Layout: React.FC<LayoutProps> = ({ children, title = 'Lazarev.Cloud' }) =>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
           gtag('config', '${GA_MEASUREMENT_ID}');
         `}
       </Script>
+
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe 
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0" 
+          width="0" 
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
 
       <header className="bg-white shadow-sm">
         <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
